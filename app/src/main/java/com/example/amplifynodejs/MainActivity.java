b554588cfd8130dc08472f9fc33a181c88d1f528
Santy Cierco <santidisturbios@gmail.com>
2023-06-02 20:08:30 +0200

    añado signin y signout pero no funciona correctamente

--- app/src/main/java/com/example/amplifynodejs/MainActivity.java
+++ app/src/main/java/com/example/amplifynodejs/MainActivity.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.amplifyframework.AmplifyException;
 import com.amplifyframework.api.aws.AWSApiPlugin;
 import com.amplifyframework.api.rest.RestOperation;
 import com.amplifyframework.api.rest.RestOptions;
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
+import com.amplifyframework.auth.cognito.result.AWSCognitoAuthSignOutResult;
+import com.amplifyframework.auth.options.AuthSignOutOptions;
 import com.amplifyframework.core.Amplify;
 
 import java.util.ArrayList;
@@ -17,6 +22,12 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<String> ls = new ArrayList<String>();
+    EditText etUsername;
+    EditText etPassword;
+    Button btnSignup;
+    Button btnSignin;
+    Button btnSignout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +44,63 @@ public class MainActivity extends AppCompatActivity {
         } catch (AmplifyException error) {
             Log.e("MyAmplifyApp", "Could not initialize Amplify.", error);
         }
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnSignin = findViewById(R.id.btnSignin);
+        btnSignout = findViewById(R.id.btnSignout);
+
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                //amplifyCognito.signIn(username, password);
+                Amplify.Auth.signIn(username, password,
+                        result ->
+                        {
+                            Log.i("AuthQuickstart", result.isSignedIn() ? "Sign in succeeded" : "Sign in not complete");
+
+
+                        },
+                        error -> Log.e("AuthQuickstart", error.toString()));
+
+                //
+
+
+            }
+        });
+
+
+
+
+
+
+        AuthSignOutOptions options = AuthSignOutOptions.builder()
+                .globalSignOut(true)
+                .build();
+
+        btnSignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //amplifyCognito.signOut();
+                Amplify.Auth.signOut(options, signOutResult -> {
+                    if (signOutResult instanceof AWSCognitoAuthSignOutResult.CompleteSignOut) {
+                        Log.i("AuthQuickstart", "ok.");
+
+                    } else if (signOutResult instanceof AWSCognitoAuthSignOutResult.PartialSignOut) {
+                        Log.i("AuthQuickstart", "ok.");
+                    } else if (signOutResult instanceof AWSCognitoAuthSignOutResult.FailedSignOut) {
+                        Log.i("AuthQuickstart", "error.");
+                    }
+                });
+
+                getTodo();
+
+            }
+        });
+
+
 
         getTodo();
 
